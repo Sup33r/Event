f201413642b84c2560edba0c0ad9f5fe4210a6b3
Super <55873818+Sup33r@users.noreply.github.com>
2024-09-28 21:21:49 +0200

    Lade till min standard boilerplate setup

--- src/main/java/live/supeer/event/Event.java
+++ src/main/java/live/supeer/event/Event.java
@@ -1,17 +1,82 @@
 package live.supeer.event;
 
+import co.aikar.commands.PaperCommandManager;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.logging.Logger;
 
 public final class Event extends JavaPlugin {
+    private static Event plugin;
+    public Logger logger = null;
+    public static EventConfiguration configuration;
+    private static LanguageManager languageManager;
+
+    public static Event getInstance() {
+        return plugin;
+    }
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        plugin = this;
+        this.logger = getLogger();
+        configuration = new EventConfiguration(this);
+        languageManager = new LanguageManager(this, "sv_se");
 
+        PaperCommandManager manager = new PaperCommandManager(this);
+        manager.enableUnstableAPI("brigadier");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static void sendMessage(@NotNull CommandSender sender, @NotNull String key, String... replacements) {
+        String message = languageManager.getValue(key, getLocale(sender), replacements);
+        if (message == null || message.isEmpty()) {
+            sender.sendMessage("§cMessage not found: " + key);
+        }
+        if (message != null && !message.isEmpty()) {
+            Component component = languageManager.getMiniMessage().deserialize(message);
+            sender.sendMessage(component);
+        }
+    }
+
+    public static String getMessage(@NotNull String key, String... replacements) {
+        String message = languageManager.getValue(key, "sv_se", replacements);
+
+        if (message != null && !message.isEmpty()) {
+            // Deserialize MiniMessage to a Component
+            Component component = languageManager.getMiniMessage().deserialize(message);
+            // Convert the Component to a legacy formatted string
+            return LegacyComponentSerializer.legacySection().serialize(component);
+        }
+        return null;
+    }
+
+    public static Component getMessageComponent(@NotNull String key, String... replacements) {
+        String message = languageManager.getValue(key, "sv_se", replacements);
+
+        if (message != null && !message.isEmpty()) {
+            return languageManager.getMiniMessage().deserialize(message);
+        }
+        return null;
+    }
+
+    public static String getRawMessage(@NotNull String key, String... replacements) {
+        return languageManager.getValue(key, "sv_se", replacements);
+    }
+
+    private static @NotNull String getLocale(@NotNull CommandSender sender) {
+        if (sender instanceof Player) {
+            return ((Player) sender).getLocale();
+        } else {
+            return Event.getInstance().getConfig().getString("settings.locale", "sv_se");
+        }
     }
 }
---
+++ src/main/java/live/supeer/event/EventConfiguration.java
@@ -0,0 +1,26 @@
+package live.supeer.event;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class EventConfiguration {
+
+    private final String sqlHost;
+    private final int sqlPort;
+    private final String sqlDatabase;
+    private final String sqlUsername;
+    private final String sqlPassword;
+
+    EventConfiguration(Event plugin) {
+        plugin.saveDefaultConfig();
+        plugin.reloadConfig();
+
+        sqlHost = plugin.getConfig().getString("sql.host");
+        sqlPort = plugin.getConfig().getInt("sql.port");
+        sqlDatabase = plugin.getConfig().getString("sql.database");
+        sqlUsername = plugin.getConfig().getString("sql.username");
+        sqlPassword = plugin.getConfig().getString("sql.password");
+    }
+}
